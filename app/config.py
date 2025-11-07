b824e1d6a735364caca244649f6fdd67ec79828f
Gaëtan Focquet <gaetan.focquet@ugent.be>
2025-11-07 14:14:49 +0100

    Update config.py

--- app/config.py
+++ app/config.py
@@ -1,10 +1,6 @@
-
 import os
 
 class Config:
-    
-    SUPABASE_URL = os.getenv("SUPABASE_URL", "https://ebokqkhwotfewvpsfemj.supabase.co")   
-    SUPABASE_KEY = os.getenv("SUPABASE_KEY", "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImVib2txa2h3b3RmZXd2cHNmZW1qIiwicm9sZSI6InNlcnZpY2Vfcm9sZSIsImlhdCI6MTc2MTY0MjQxOCwiZXhwIjoyMDc3MjE4NDE4fQ.THk44izi0OPqEp6T5VMazLBVqd7J3NlKWRj44RUBGpA")   
-
-   
-    SECRET_KEY = os.getenv("SECRET_KEY", "supergeheimesleutel")  
+    SUPABASE_URL = os.getenv("SUPABASE_URL")   # Supabase project URL
+    SUPABASE_KEY = os.getenv("SUPABASE_KEY")   # Service role key voor server-side toegang
+    SECRET_KEY = os.getenv("SECRET_KEY", "supergeheimesleutel")  # Flask secret key
