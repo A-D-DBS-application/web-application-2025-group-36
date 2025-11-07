421ca6d29628928c4cef979f4dfa8d1ebab7e407
Emiel Van hauwermeiren <vanhauwermeirenemiel@gmail.com>
2025-11-07 16:46:55 +0100

    Update config.py

--- app/config.py
+++ app/config.py
@@ -1,5 +1,13 @@
+import os
+from dotenv import load_dotenv
+
+# Load environment variables from .env
+load_dotenv()
 
 class Config:
-    SUPABASE_URL = "https://ebokqkhwotfewvpsfemj.supabase.co"  
-    SUPABASE_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImVib2txa2h3b3RmZXd2cHNmZW1qIiwicm9sZSI6InNlcnZpY2Vfcm9sZSIsImlhdCI6MTc2MTY0MjQxOCwiZXhwIjoyMDc3MjE4NDE4fQ.THk44izi0OPqEp6T5VMazLBVqd7J3NlKWRj44RUBGpA"  # service_role key
-    SECRET_KEY = "supergeheimesleutel"  
+    SECRET_KEY = os.getenv("SECRET_KEY", "dev_key")  # fallback if not set
+    SQLALCHEMY_DATABASE_URI = (
+        f"postgresql+psycopg2://{os.getenv('USER')}:{os.getenv('PASSWORD')}"
+        f"@{os.getenv('HOST')}:{os.getenv('PORT')}/{os.getenv('DBNAME')}?sslmode=require"
+    )
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
