9dc82b2dab9289aa11d8adcc154bfaf546b4228c
Emiel Van hauwermeiren <vanhauwermeirenemiel@gmail.com>
2025-11-11 22:52:10 +0100

    Update routes.py

--- app/routes.py
+++ app/routes.py
@@ -1,64 +1,57 @@
 from flask import Blueprint, render_template, request, redirect, url_for, flash
-from .config import Config, DUMMY_PAPERS, DUMMY_COMPANIES, LINKS
+from .models import db, Paper, Company, PaperCompany
 
 bp = Blueprint("main", __name__)
 
 @bp.route("/")
 def index():
-    """
-    Landing page met 2 knoppen:
-    - 'Ik ben schrijver' → /author
-    - 'Ik ben bedrijf'  → /company
-    """
+    """Landing page with navigation"""
     return render_template("index.html")
 
 
 @bp.route("/author", methods=["GET", "POST"])
 def author():
-    """
-    Eenvoudige 'schrijver' pagina:
-    - toont lijst met eigen/alle papers (dummy)
-    - klein formulier om paper aan bedrijf te koppelen (MVP)
-    """
+    """Author page: list papers, link paper to company"""
+    papers = Paper.query.all()
+    companies = Company.query.all()
+
     if request.method == "POST":
         paper_id = int(request.form.get("paper_id"))
         company_id = int(request.form.get("company_id"))
-        LINKS.append((paper_id, company_id))
-        flash("Paper succesvol gelinkt aan bedrijf (dummy, in-memory).", "success")
+
+        existing_link = PaperCompany.query.filter_by(paper_id=paper_id, company_id=company_id).first()
+        if existing_link:
+            flash("Deze paper is al gelinkt aan dit bedrijf.", "warning")
+        else:
+            link = PaperCompany(paper_id=paper_id, company_id=company_id)
+            db.session.add(link)
+            db.session.commit()
+            flash("Paper succesvol gelinkt aan bedrijf!", "success")
+
         return redirect(url_for("main.author"))
 
-    # Bouw een overzicht van bestaande links voor weergave
-    linked = []
-    for (p_id, c_id) in LINKS:
-        p = next((x for x in DUMMY_PAPERS if x["id"] == p_id), None)
-        c = next((x for x in DUMMY_COMPANIES if x["id"] == c_id), None)
-        if p and c:
-            linked.append({"paper": p["title"], "company": c["name"]})
+    # Build a list of linked pairs
+    linked = [
+        {"paper": link.paper.title, "company": link.company.name}
+        for link in PaperCompany.query.all()
+    ]
 
     return render_template(
         "author.html",
-        papers=DUMMY_PAPERS,
-        companies=DUMMY_COMPANIES,
+        papers=papers,
+        companies=companies,
         links=linked,
     )
 
 
 @bp.route("/company")
 def company():
-    """
-    Eenvoudige 'bedrijf' pagina:
-    - toont bedrijven (dummy)
-    - toont welke papers al gelinkt zijn (dummy)
-    """
-    # Bouw per bedrijf een lijst met gelinkte papers
+    """Company page: show companies and linked papers"""
+    companies = Company.query.all()
+
     company_view = []
-    for comp in DUMMY_COMPANIES:
-        comp_links = []
-        for (p_id, c_id) in LINKS:
-            if c_id == comp["id"]:
-                p = next((x for x in DUMMY_PAPERS if x["id"] == p_id), None)
-                if p:
-                    comp_links.append(p["title"])
-        company_view.append({"company": comp["name"], "papers": comp_links})
+    for comp in companies:
+        linked_papers = [link.paper.title for link in comp.paper_links]
+        company_view.append({"company": comp.name, "papers": linked_papers})
 
     return render_template("company.html", company_view=company_view)
