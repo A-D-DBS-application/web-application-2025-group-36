bfeac38617a654905a59577dd02186c763112a8a
Gaëtan Focquet <gaetan.focquet@ugent.be>
2025-12-08 18:28:36 +0100

    Revert "Update ddl.sql"
    This reverts commit 44df568c298863dc3838e358c6a665f8833d4b25.

--- database/ddl.sql
+++ database/ddl.sql
@@ -69,7 +69,7 @@ CREATE TABLE complaint (
 );
 
 -- Alembic versie tabel
-CREATE TABLE IF NOT EXISTS alembic_version (
+CREATE TABLE alembic_version (
     version_num VARCHAR(50) NOT NULL,
     PRIMARY KEY (version_num)
 );
